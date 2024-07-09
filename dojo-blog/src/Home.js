31dfd3fca71df70184ea3b17e4b474acb6482136
iyanubruce <iyanuoluwa938@gmail.com>
2024-07-09 20:40:38 +0100

    adding more dojo blog files

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,35 +1,16 @@
-
 import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
 
-   
-  const [blogs, setBlogs] = useState();
-
-    const [name, setName] = useState('mario');
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blogs => blogs.id !== id);
-        setBlogs(newBlogs);
-    }
-
-    useEffect(() => {
-      fetch("http://localhost:8000/blogs")
-      .then (res => {
-        return res.json;
-      })
-      .then (data => {
-        console.log('data')
-      })
-    }, []);
-    
   return (
     <div className="home">
-      {/* <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} /> */}
-      
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
     </div>
-    
   );
 }
  
--- dojo-blog/src/Navbar.js
+++ dojo-blog/src/Navbar.js
@@ -1,14 +1,17 @@
 const Navbar = () => {
-    return(
-        <nav className="navbar">
-            <h1> Dojo blog</h1>
-            <div className="links">
-                <a href="">Home</a>
-                <a href="">Blog</a>
-                <a href="">About</a>
-            </div>
-        </nav>
+    return (
+      <nav className="navbar">
+        <h1>The Dojo Blog</h1>
+        <div className="links">
+          <a href="/">Home</a>
+          <a href="/create" style={{ 
+            color: 'white', 
+            backgroundColor: '#f1356d',
+            borderRadius: '8px' 
+          }}>New Blog</a>
+        </div>
+      </nav>
     );
-}
-
-export default Navbar
+  }
+   
+  export default Navbar;
